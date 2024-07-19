f25372e4f1c0a1215d21d79d118e46b51c14fcfd
st119848 <st119848@ait.asia>
2024-07-19 13:12:31 +0700

    Change string pointer to integer

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
 	// emp := Employee{"John", "Wicker", 10}
 	// emp.Display()
@@ -13,20 +18,29 @@ func main() {
 	// rect.increaseWidth(10.0)
 	// fmt.Println(rect)
 
-	var emp Human = Employee{name: "Rob Pike", designation: "Engineer"}
-	var emp2 Human = Employee{name: "John", designation: "Scientist"}
+	// var emp Human = Employee{name: "Rob Pike", designation: "Engineer"}
+	// var emp2 Human = Employee{name: "John", designation: "Scientist"}
 
-	var cont Human = Contractor{name: "XYZ", weeklyHours: 35}
-	var cont2 Human = Contractor{name: "ABC", weeklyHours: 40}
+	// var cont Human = Contractor{name: "XYZ", weeklyHours: 35}
+	// var cont2 Human = Contractor{name: "ABC", weeklyHours: 40}
 
-	humans := []Human{emp, emp2, cont, cont2}
+	// humans := []Human{emp, emp2, cont, cont2}
 
-	for i := 0; i < len(humans); i++ {
-		humans[i].Display()
-	}
+	// for i := 0; i < len(humans); i++ {
+	// 	humans[i].Display()
+	// }
 
-	for _, human := range humans {
-		human.Display()
-	}
+	// for _, human := range humans {
+	// 	human.Display()
+	// }
+	var message int = 15
+	fmt.Println("Before function call - "+strconv.Itoa(message), message, message)
+	displayMessagePointer(&message)
+	fmt.Println("After function call - " + strconv.Itoa(message))
 
+}
+func displayMessagePointer(message *int) {
+	fmt.Println("Before update - " + strconv.Itoa(*message))
+	*message = 20
+	fmt.Println("After update - " + strconv.Itoa(*message))
 }

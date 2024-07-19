9881ba1e6eb9d9e97af4f17b76661323bbc5c650
st119848 <st119848@ait.asia>
2024-07-19 11:31:26 +0700

    Add Sample for Human Slice

--- main.go
+++ main.go
@@ -14,8 +14,19 @@ func main() {
 	// fmt.Println(rect)
 
 	var emp Human = Employee{name: "Rob Pike", designation: "Engineer"}
-	emp.Display()
+	var emp2 Human = Employee{name: "John", designation: "Scientist"}
+
 	var cont Human = Contractor{name: "XYZ", weeklyHours: 35}
-	cont.Display()
+	var cont2 Human = Contractor{name: "ABC", weeklyHours: 40}
+
+	humans := []Human{emp, emp2, cont, cont2}
+
+	for i := 0; i < len(humans); i++ {
+		humans[i].Display()
+	}
+
+	for _, human := range humans {
+		human.Display()
+	}
 
 }
